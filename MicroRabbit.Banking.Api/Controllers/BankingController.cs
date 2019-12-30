c03b67affa2beb6fe1b73884ae46f478f3420c8b
DanielQuintana <danielquintana90@gmail.com>
2019-12-30 11:35:53 -0500

    Implemented Publish and SendCommand for Banking Microservice

--- MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,4 +1,5 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -20,6 +21,14 @@ namespace MicroRabbit.Banking.Api.Controllers
         public ActionResult<IEnumerable<Account>> Get()
         {
             return Ok(_accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+
+            return Ok(accountTransfer);
         }
     }
 }
--- MicroRabbit.IoC/DependencyContainer.cs
+++ MicroRabbit.IoC/DependencyContainer.cs
@@ -1,7 +1,10 @@
-﻿using MicroRabbit.Banking.Application.Interfaces;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
@@ -13,16 +16,19 @@ namespace MicroRabbit.Infra.IoC
     {
         public static void Register(IServiceCollection services)
         {
-            services.AddTransient<BankingDbContext>();
-
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Domain Banking Commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
 
             //Banking Microservice - Application Services
             services.AddTransient<IAccountService, AccountService>();
 
             //Banking Microservice - Data
-            services.AddTransient<IAccountRepository, AccountRepository>();            
+            services.AddTransient<IAccountRepository, AccountRepository>();
+
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
