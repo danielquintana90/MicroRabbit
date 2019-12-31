8ffc3223c31d26980948e3a52900eefe2129ae73
DanielQuintana <danielquintana90@gmail.com>
2019-12-31 01:48:44 -0500

    Implemented Solution

---
+++ MicroRabbir.MVC/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Services;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, ITransferService transferService)
+        {
+            _logger = logger;
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDTO transferDTO = new TransferDTO() { 
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount  = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDTO);
+
+            return View("Index");
+        }
+    }
+}
---
+++ MicroRabbir.MVC/Services/TransferService.cs
@@ -0,0 +1,28 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDTO transderDTO)
+        {
+            var url = "https://localhost:5001/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transderDTO), Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(url, transferContent);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
